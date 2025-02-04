9cd34fea698638676ea344e1d9507d0ef4b78ec2
JulianaSalfity <156864339+JulianaSalfity@users.noreply.github.com>
2025-02-04 16:21:06 -0300

    Add files via upload

--- claseSQL-TareaClase-Template.py
+++ claseSQL-TareaClase-Template.py
@@ -212,7 +212,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a1.- Listar a los alumnos que cursan BDs o TLENG
 
 consultaSQL = """
-
+                    SELECT DISTINCT *
+                    FROM alumnosBD
+                UNION
+                    SELECT DISTINCT *
+                    FROM alumnosTLeng
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -222,6 +226,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a2.- Listar a los alumnos que cursan BDs o TLENG (usando UNION ALL)
 
 consultaSQL = """
+                      SELECT DISTINCT *
+                      FROM alumnosBD
+                  UNION ALL
+                      SELECT DISTINCT *
+                      FROM alumnosTLeng
 
               """
 
@@ -231,6 +240,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # b.- Listar a los alumnos que cursan simultáneamente BDs y TLENG
 
 consultaSQL = """
+                    SELECT DISTINCT *
+                    FROM alumnosBD
+                INTERSECT
+                    SELECT DISTINCT *
+                    FROM alumnosTLeng
 
               """
 
@@ -240,6 +254,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # c.- Listar a los alumnos que cursan BDs y no cursan TLENG 
 
 consultaSQL = """
+                SELECT DISTINCT *
+                FROM alumnosBD
+            EXCEPT
+                SELECT DISTINCT *
+                FROM alumnosTLeng
 
               """
 
@@ -262,7 +281,12 @@ dataframeResultado = dd.sql(consultaSQL).df()
 #=============================================================================
 # Ejercicio 02.1.- Devolver los números de vuelo que tienen reservas generadas (utilizar intersección)
 consultaSQL = """
-
+                    SELECT DISTINCT numero
+                    FROM vuelo
+                INTERSECT
+                    SELECT DISTINCT nrovuelo
+                    FROM reserva
+                    
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -270,7 +294,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 #%%-----------
 # Ejercicio 02.2.- Devolver los números de vuelo que aún no tienen reservas
 consultaSQL = """
-
+                        SELECT DISTINCT numero
+                        FROM vuelo
+                    EXCEPT
+                        SELECT DISTINCT nrovuelo
+                        FROM reserva
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -278,7 +306,14 @@ dataframeResultado = dd.sql(consultaSQL).df()
 #%%-----------
 # Ejercicio 02.3.- Retornar los códigos de aeropuerto de los que parten o arriban los vuelos
 consultaSQL = """
-
+                        SELECT DISTINCT codigo
+                        FROM aeropuerto
+                    INTERSECT
+                        (SELECT DISTINCT origen
+                        FROM vuelo
+                    UNION
+                        SELECT DISTINCT destino
+                        FROM vuelo)
               """
               
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -301,7 +336,9 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a1.- Listar el producto cartesiano entre las tablas persona y nacionalidades
 
 consultaSQL = """
-
+                SELECT DISTINCT *
+                FROM persona
+            CROSS JOIN nacionalidades
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -311,7 +348,8 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a2.- Listar el producto cartesiano entre las tablas persona y nacionalidades (sin usar CROSS JOIN)
 
 consultaSQL = """
-
+                    SELECT DISTINCT *
+                    FROM persona, nacionalidades
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -325,6 +363,10 @@ persona        = pd.read_csv(carpeta+"persona_ejemplosJoin.csv")
 # b1.- Vincular las tablas persona y nacionalidades a través de un INNER JOIN
 
 consultaSQL = """
+                    SELECT DISTINCT *
+                    FROM persona
+                INNER JOIN nacionalidades
+                ON nacionalidad = IDN
 
               """
 
@@ -334,7 +376,9 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # b2.- Vincular las tablas persona y nacionalidades (sin usar INNER JOIN)
 
 consultaSQL = """
-
+                    SELECT DISTINCT *
+                    FROM persona, nacionalidades
+                    WHERE nacionalidad = IDN
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -343,6 +387,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # c.- Vincular las tablas persona y nacionalidades a través de un LEFT OUTER JOIN
 
 consultaSQL = """
+                    SELECT DISTINCT *
+                    FROM persona
+                LEFT OUTER JOIN nacionalidades
+                ON nacionalidad = IDN
 
               """
 
@@ -354,7 +402,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a.- Vincular las tablas Se_inscribe_en y Materia. Mostrar sólo LU y Nombre de materia
 
 consultaSQL = """
-
+                    SELECT DISTINCT LU, nombre
+                    FROM se_inscribe_en 
+                    INNER JOIN materia
+                    ON materia.codigo_materia = se_inscribe_en.codigo_materia
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -378,7 +429,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # Ejercicio 03.1.- Devolver el nombre de la ciudad de partida del vuelo número 165
 
 consultaSQL = """
-
+                    SELECT DISTINCT ciudad 
+                    FROM aeropuerto
+                    INNER JOIN vuelo
+                    ON origen = codigo
+                    WHERE numero = 165
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -387,7 +442,12 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # Ejercicio 03.2.- Retornar el nombre de las personas que realizaron reservas a un valor menor a $200
 
 consultaSQL = """
-
+                SELECT DISTINCT nombre 
+                FROM pasajero
+                INNER JOIN reserva
+                ON reserva.DNI = pasajero.DNI
+                WHERE precio<'200'
+                
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -396,15 +456,23 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # Ejercicio 03.3.- Obtener Nombre, Fecha y Destino del Viaje de todos los pasajeros que vuelan desde Madrid
 
 vuelosAMadrid = dd.sql("""
-
+                       SELECT DISTINCT * 
+                       FROM vuelo
+                       WHERE Origen = 'MAD'
               """).df()
 
 dniPersonasDesdeMadrid = dd.sql("""
-
+                                SELECT DISTINCT *
+                                FROM reserva
+                                INNER JOIN vuelosAMadrid
+                                ON nrovuelo = numero
               """).df()
 
 consultaSQL = """
-
+                SELECT DISTINCT Nombre, Fecha, Destino
+                FROM dniPersonasDesdeMadrid AS dp
+                INNER JOIN pasajero AS p
+                ON dp.dni = p.dni
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -427,7 +495,9 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a.- Vincular las tablas Reserva, Pasajero y Vuelo. Mostrar sólo Fecha de reserva, hora de salida del vuelo y nombre de pasajero.
     
 consultaSQL = """
-
+                SELECT DISTINCT fecha, salida, nombre
+                FROM reserva AS r,pasajero AS p,vuelo AS v
+                WHERE r.dni = p.dni AND r.nrovuelo = v.numero
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -439,7 +509,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a.- Vincular (JOIN)  EmpleadoRol y RolProyecto para obtener la tabla original EmpleadoRolProyecto
     
 consultaSQL = """
-
+                SELECT DISTINCT er.empleado, er.rol, rp.proyecto
+                FROM empleadoRol AS er
+                INNER JOIN rolProyecto AS rp
+                ON rp.rol = er.rol
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -450,6 +523,8 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a.- Usando sólo SELECT contar cuántos exámenes fueron rendidos (en total)
     
 consultaSQL = """
+                SELECT count(*) AS cantidadExamenes
+                FROM examen
 
               """
 
@@ -460,7 +535,9 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # b1.- Usando sólo SELECT contar cuántos exámenes fueron rendidos en cada Instancia
     
 consultaSQL = """
-
+                SELECT Instancia, count(*) AS Asistieron
+                FROM examen
+                GROUP BY Instancia
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -470,7 +547,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # b2.- Usando sólo SELECT contar cuántos exámenes fueron rendidos en cada Instancia (ordenado por instancia)
     
 consultaSQL = """
-
+                SELECT Instancia, count(*) AS Asistieron
+                FROM examen
+                GROUP BY Instancia
+                ORDER BY Instancia DESC
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -480,7 +560,11 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # b3.- Ídem ejercicio anterior, pero mostrar sólo las instancias a las que asistieron menos de 4 Estudiantes
     
 consultaSQL = """
-
+                SELECT Instancia, count(*) AS Asistieron
+                FROM examen
+                GROUP BY Instancia
+                HAVING Asistieron < 4                
+                ORDER BY Instancia 
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -489,7 +573,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # c.- Mostrar el promedio de edad de los estudiantes en cada instancia de examen
     
 consultaSQL = """
-
+                SELECT instancia,AVG(edad) AS promedio_edad
+                FROM examen
+                GROUP BY Instancia
+                ORDER BY Instancia
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -501,16 +588,25 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a1.- Mostrar cuál fue el promedio de notas en cada instancia de examen, sólo para instancias de parcial.
     
 consultaSQL = """
+                SELECT Instancia,AVG(nota) AS promedio_nota
+                FROM examen
+                GROUP BY Instancia
+                HAVING Instancia = 'Parcial-01' OR Instancia = 'Parcial-02' 
+                ORDER BY Instancia
 
               """
-
+#HAVING Instancia LIKE 'Parcial%'
 dataframeResultado = dd.sql(consultaSQL).df()
 
 #%%-----------
 # a2.- Mostrar cuál fue el promedio de notas en cada instancia de examen, sólo para instancias de parcial. Esta vez usando LIKE.
     
 consultaSQL = """
-
+                     SELECT Instancia,AVG(nota) AS promedio_nota
+                     FROM examen
+                     GROUP BY Instancia
+                     HAVING Instancia LIKE 'Parcial%'
+                     ORDER BY Instancia
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -522,7 +618,14 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a1.- Listar a cada alumno que rindió el Parcial-01 y decir si aprobó o no (se aprueba con nota >=4).
     
 consultaSQL = """
-
+                SELECT nombre, nota, 
+                    CASE WHEN nota >= 4 
+                        THEN 'aprobó'
+                        ELSE 'no aprobó'
+                    END AS Estado
+                FROM examen
+                WHERE Instancia = 'Parcial-01'
+                ORDER BY Estado, Nombre
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -532,7 +635,15 @@ dataframeResultado = dd.sql(consultaSQL).df()
 # a2.- Modificar la consulta anterior para que informe cuántos estudiantes aprobaron/reprobaron en cada instancia.
     
 consultaSQL = """
-
+                SELECT Instancia,
+                    CASE WHEN nota >= 4 
+                        THEN 'aprobó'
+                        ELSE 'no aprobó'
+                    END AS Estado,
+                COUNT(*) AS Cantidad
+                FROM examen
+                GROUP BY Instancia, Estado
+                ORDER BY Instancia, Estado
               """
 
 dataframeResultado = dd.sql(consultaSQL).df()
@@ -544,7 +655,10 @@ dataframeResultado = dd.sql(consultaSQL).df()
 #a.- Listar los alumnos que en cada instancia obtuvieron una nota mayor al promedio de dicha instancia
 
 consultaSQL = """
-
+                     SELECT Instancia,AVG(nota) AS promedio_nota
+                     FROM examen
+                     GROUP BY Instancia
+                     ORDER BY Instancia
               """
 
 
